95b81051e1d834f04570352390440b5effc6c5a1
sadikahmetaydin <sadikahmetaydin@gmail.com>
2024-11-12 20:11:28 +0300

    create editListing page

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ import PrivateRoute from "./components/PrivateRoute";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
 import Header from "./components/Header";
+import CreateListing from "./pages/CreateListing";
+import EditListing from "./pages/EditListing";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import CreateListing from "./pages/CreateListing";
 
 function App() {
   return (
@@ -27,6 +28,9 @@ function App() {
           <Route path="/offers" element={<Offers />} />
           <Route path="create-listing" element={<PrivateRoute />}>
             <Route path="/create-listing" element={<CreateListing />} />
+          </Route>
+          <Route path="edit-listing" element={<PrivateRoute />}>
+            <Route path="/edit-listing/:listingId" element={<EditListing />} />
           </Route>
         </Routes>
       </Router>
